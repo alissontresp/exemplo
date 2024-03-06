09765e609b918602932f185f062ff3be95565777
alissontresp <alissonhulw@gmail.com>
2024-03-06 15:45:34 -0300

    Initial commit

---
+++ R/app_server.R
@@ -0,0 +1,17 @@
+#' The application server-side
+#'
+#' @param input,output,session Internal parameters for {shiny}.
+#'     DO NOT REMOVE.
+#' @import shiny
+#' @noRd
+app_server <- function(input, output, session) {
+  # Your application server logic
+  dados_pls <- read.csv2(
+    app_sys("/inst/dados_pls.csv")
+  )
+
+  output$grafico <- renderPlot({
+   plot(dados_pls$ca)
+  })
+
+}
